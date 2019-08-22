12346c6551c6455ab44a99dcf434eef15cfacaba
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-22 14:32:56 -0700

    Created form with required info along with a checkbox and submit button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import From from './Components/Form'
+import NewForm from './Components/Form'
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <NewForm />
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,3 +3,70 @@ import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import '../App.css';
+
+const NewForm = ({errors, touched, values, status}) => {
+    const [users, setUsers] = useState([]);
+    console.log(users)
+
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
+
+    return (
+        <div>
+            <h1>Please fill out the form below</h1>
+            <Form className="form">
+                <Field className="field" component="input" type="text" name="name" placeholder="Enter Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+
+                <Field className="role" component="select" type="text" name="role">
+                <option>Select Role</option>
+                <option value="dev">Developer</option>
+                <option value="ux">Designer</option>
+                <option value="tl">Team Lead</option>
+                </Field>
+                {touched.role && errors.role && (
+                    <p className="error">{errors.role}</p>
+                )}
+
+                <Field className="field" component="input" type="text" name="email" placeholder="Enter Email" />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
+
+                <Field className="field" component="input" type="password" name="password" placeholder="Enter Password" />
+                {touched.password && errors.password && (
+                    <p className="errors">{errors.password}</p>
+                )}
+
+                <label className="checkbox">Remember Me
+                <Field name="remember" type="checkbox" checked={values.remember} />
+                <span className="checkmark" />
+                {touched.remember && errors.remember && (
+                    <p className="error">{errors.remember}</p>
+                )}
+                </label>
+
+                <button type="submit">
+                </button>
+            </Form>
+
+
+
+
+
+
+
+
+        </div>
+    )
+
+
+
+}
+
+export default NewForm;
